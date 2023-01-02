d97d1ba82030579d70322ae68a3fe8a29fa4cc29
Sevilay Turkoglu <sevilayt1975@gmail.com>
2023-01-02 21:10:59 +0200

    geceson

--- src/day27_overrding/DGrandParent.java
+++ src/day27_overrding/DGrandParent.java
@@ -1,5 +1,11 @@
 package day27_overrding;
 
 public class DGrandParent {
+    protected void method1(){
+        System.out.println("GP method1");
+    }
 
+    protected void method2(){
+        System.out.println("GP method2");
+    }
 }
--- src/day27_overrding/EParent.java
+++ src/day27_overrding/EParent.java
@@ -1,4 +1,20 @@
 package day27_overrding;
 
 public class EParent extends DGrandParent{
+    @Override
+    protected void method1() {
+        System.out.println("P method1");
+
+        // @override notasyonu overridden method ile overriding method'u ilisiklendirir
+        // eger overriden method'da bu iliskiyi bozacak bir degisiklik yapilirsa
+        // @override CTE verir
+    }
+
+    protected void method2(){
+        System.out.println("P method2");
+    }
+
+    private void method3(){
+        System.out.println("P method3");
+    }
 }
